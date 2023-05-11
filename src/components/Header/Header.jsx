49f12bad370494502b95f6e635db18458d15f755
Leandro Leme dos Santos <leandrolemesantos@gmail.com>
2023-05-11 20:23:56 -0300

    129º dia de CTD - Avançando no HTML/CSS

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import SearchBar from './SearchBar/SearchBar';
 
 import './Header.css';
+import CartButton from '../CartButton/CartButton';
 
 function Header() {
   return (
     <header className="header">
       <div className="container">
         <SearchBar />
-        <button>Cart</button>
+        <CartButton />
       </div>
     </header>
   );
